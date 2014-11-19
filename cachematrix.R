ddd1e070514b11586d97500ad5232f86ba44295e
Inge <i.vanengeland@fontys.nl>
2014-11-19 11:32:19 +0100

    Programming assignment 2
    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## With MakeCachMatrix you can add a matrix to the cache, it will be saved
+## as a list. 
+## To use it, first create a matrix, then pass this matrix as
+## an argument to makeCacheMatrix and store the result (which will be a list)
+## in a variable. Call cacheSolve and pass the list in the variable you just
+## stored as the argument. The inversed matrix will be returned.
 
-## Write a short comment describing this function
-
+## Saves a matrix in the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- null
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## returns the inversed matrix. When the matrix is already in cache, 
+## a message will be displayed. And the matrix will be inversed. If it's
+## not in the cache, the matrix will be inversed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
